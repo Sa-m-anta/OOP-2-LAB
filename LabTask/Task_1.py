397965068bcfaf28aa925f3e834d31540adde5a1
Samanta Islam Zerin <samantaislam106@gmail.com>
2024-12-04 17:43:42 +0600

    Add files via upload

---
+++ LabTask/Task_1.py
@@ -0,0 +1,9 @@
+# Function to calculate (a + b)^2
+
+# Taking input
+a = int(input("Enter value for a: "))
+b = int(input("Enter value for b: "))
+
+def calculate_formula(a, b):
+    return (a + b)**2
+print("Result using the function:", calculate_formula(a, b))
